106be06fd3cf9c4213d532b367bfe816601cf1f1
Daniel-utpl <fdcuricama@utpl.edu.ec>
2025-05-22 23:20:26 -0500

    TallerResuelto

--- streamlit_app_001.py
+++ streamlit_app_001.py
@@ -3,12 +3,12 @@ import mlflow.sklearn
 import numpy as np
 
 st.title("Predicción de Diabetes")
-st.title("Freddy Daniel Curicama Curicam")
+st.title("Freddy Daniel Curicama Curicama")
 # Configurar conexión con MLflow Tracking Server
-pass
+mlflow.set_tracking_uri("http://localhost:9090")
 
 # Cargar modelo desde el Model Registry, revise el ejemplo de flask
-pass
+model = mlflow.sklearn.load_model("models:/DemoDiabetes/1")
 
 # Deslizadores para cada input del modelo
 pregnancies = st.slider("Número de embarazos", 0, 20, 1)
